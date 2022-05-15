da71967ee5389c0fc95019a4f391c7383bace011
Oleksii Bezchastnyi <you@example.comobezchastnyi@gmail.com>
2022-05-15 13:56:35 +0300

    client: add adding components; server: refactor

---
+++ src/Server/Server/Controllers/OperationController.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server
+{
+    [ApiController]
+    public class OperationController : Controller
+    {
+        [HttpGet]
+        [Route("Read")]
+        public IActionResult Read()
+        {
+            return new JsonResult(new ServerDbContext().Product.ToList());
+        }
+        
+        [HttpGet]
+        [Route("Write/{id:int}/{purchase}/{done:bool}/{price:int}")]
+        public IActionResult Write(int id, string purchase, bool done, int price)
+        {
+            try
+            {
+                var db = new ServerDbContext();
+                db.Product.Add(new Product
+                {
+                    Id = id,
+                    Done = done,
+                    Price = price,
+                    Purchase = purchase
+                });
+                db.SaveChanges();
+            }
+            catch
+            {
+                return new JsonResult("Bad");
+            }
+
+            return new JsonResult("OK");
+        }
+    }
+}
---
+++ src/Server/Server/Db/ServerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Models;
+
+namespace Server
+{
+    public class ServerDbContext : DbContext
+    {
+        public virtual DbSet<Product> Product { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=postgres");
+        }
+    }
+}
