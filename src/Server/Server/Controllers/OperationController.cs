6982a1354fa7accfd1bbd4af91d26ac99229ff13
Oleksii Bezchastnyi <you@example.comobezchastnyi@gmail.com>
2022-05-19 12:52:22 +0300

    server: fix write OperationController.cs

--- src/Server/Server/Controllers/OperationController.cs
+++ src/Server/Server/Controllers/OperationController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Server.Db;
 using Server.Models;
@@ -12,28 +13,34 @@ namespace Server.Controllers
         [Route("Read")]
         public IActionResult Read()
         {
-            return new JsonResult(new ServerDbContext().Product.ToList());
+            try
+            {
+                return new JsonResult(new ServerDbContext().Product.ToList());
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult($"Bad request: {ex.Message}");
+            }
         }
         
         [HttpGet]
-        [Route("Write/{id:int}/{purchase}/{done:bool}/{price:int}")]
-        public IActionResult Write(int id, string purchase, bool done, int price)
+        [Route("Write/{purchase}/{done:bool}/{price:int}")]
+        public IActionResult Write(string purchase, bool done, int price)
         {
             try
             {
                 var db = new ServerDbContext();
                 db.Product.Add(new Product
                 {
-                    Id = id,
                     Done = done,
                     Price = price,
                     Purchase = purchase
                 });
                 db.SaveChanges();
             }
-            catch
+            catch (Exception ex)
             {
-                return new JsonResult("Bad");
+                return new JsonResult($"Bad request: {ex.Message}");
             }
 
             return new JsonResult("OK");
